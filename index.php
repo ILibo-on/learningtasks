850adcc5676aec26b6689da599a5cfe862f6370a
ILibo-on <100105582+ILibo-on@users.noreply.github.com>
2022-04-02 15:08:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Welcome to my Website! I am {Idrian Ilgin H. Libo-on}</h1>
     <ul>
         <li><a href="index.html">Home</a></li>    
-        <li><a href="C:/Users/Acer/Desktop/ITC101/index.html">Learning Task 2.1</a></li>
+        <li><a href="/C:/Users/Acer/Desktop/ITC101/index.html">Learning Task 2.1</a></li>
         
     </ul>
     </body>
