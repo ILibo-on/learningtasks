92e1c183fc7b945fa7f6b20debf6e51bd2c817ac
ILibo-on <100105582+ILibo-on@users.noreply.github.com>
2022-05-21 23:14:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,54 @@
-<!DOCTYPE html>
+<?php
+$page = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page'] : ''; 
+?>
+<! DOCTYPE html>
 <html>
-    <head>
-         <title>My Home Page</title>
-    </head>     
+     <head>
+          <title>My Page</title>
+           <style>
+           <?php include "stylesheet.css" ?>
+           </style>
+    </head>
     <body>
-    <h1>Welcome to my Website! I am {Idrian Ilgin H. Libo-on}</h1>
+      <p>Please fill up what is asked before opening the website.</p>
+
+        
+    <script src="script.js">
+     
+     </script>
+         <div id="wrapper">
+           <div id="header">
+               <h1>CASE STUDY:
+
+WEB DESIGNER PORTFOLIO </h1>
+               <div id="navigation">
+     
     <ul>
-        <li><a href="index.php">Home</a></li>    
-        <li><a href="/C:/Users/Acer/Desktop/ITC101/index.html">Learning Task 2.1</a></li>
-        <li><a href="LT4/index.html">Learning Task 4.1</a></li>
-         <li><a href="LT3/index.html">Learning Task 3.1</a></li>
-         <li><a href="LT5/index.html">Learning Task 5.1</a></li>
-       
-    </ul>
+        <li><a href="index.php">Home</a></li>
+         <li><a href="LT2/index.html" target="_blank">Learning Task 2.1</a></li>
+        <li><a href="LT4/index.html" target="_blank">Learning Task 4.1</a></li>
+         <li><a href="LT3/index.html" target="_blank">Learning Task 3.1</a></li>
+         <li><a href="LT5/index.html" target="_blank">Learning Task 5.1</a></li>
+        <li><a href="Midterm/index.html" target="_blank">Midterm</a></li>
+        </ul>
+        <?php
+           switch($page) {
+             
+           }
+        ?>
+                    </div>
+            </div>
+               <div id="content">
+                   <div id="content wrapper">
+                       <div id="about - text">
+                       </div>
+                       
+                   </div>
+               </div>
+               <div id="footer">
+                   
+                
+               </div>
+            </div>
     </body>
 </html>
